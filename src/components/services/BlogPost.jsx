50cc1b87208ac8f27311f32419760d83a884cf9f
supreMe7492 <supremeraut94@gmail.com>
2026-05-03 16:39:49 +0545

    added publish blog fumction

--- src/components/services/BlogPost.jsx
+++ src/components/services/BlogPost.jsx
@@ -44,4 +44,19 @@ async function editPost(id, postData) {
   }
   return response;
 }
-export { getPosts, createPost, editPost };
+
+async function publishPost(id) {
+  const data = await fetch(`http://localhost:3000/author/posts/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  const response = await data.json();
+  if (!data.ok) {
+    throw new Error(response.error.message || "something wnet wrong");
+  }
+  return response;
+}
+export { getPosts, createPost, editPost, publishPost };
